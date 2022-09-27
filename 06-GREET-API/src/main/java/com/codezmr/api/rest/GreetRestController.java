3171dc0e6d83e330ad867986398cc32129f7bafe
codezmr <mdzamiruddin.zmr@gmail.com>
2022-09-27 11:58:05 +0530

    Grreting message api

---
+++ 06-GREET-API/src/main/java/com/codezmr/api/rest/GreetRestController.java
@@ -0,0 +1,15 @@
+package com.codezmr.api.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+	
+	@GetMapping("/greet")
+	public String getGreetMessage() {
+		
+		String message = "Good Morning..!!";
+		return message;
+	}
+}
